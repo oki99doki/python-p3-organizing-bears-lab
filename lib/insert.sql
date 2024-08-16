a78c3b44a929844390f1dcd984c2d17f0a677664
Stefan Klinkert <s.klinkert@gmail.com>
2024-08-16 17:35:36 +0200

    complete Bears Lab

---
+++ lib/insert.sql
@@ -0,0 +1,22 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES 
+    ('Mr. Chocolate', 8 ,'M', 'brown', 'wild', True),
+    ('Rowdy', 15 ,'F', 'black', 'wild', True),
+    ('Tabitha', 4 ,'F', 'white', 'playful', True),
+    ('Seargant Brown', 12 ,'M', 'brown', 'composed', True),
+    ('Melissa', 12 ,'F', 'black', 'tame', True),
+    ('Grinch', 18 ,'M', 'brown', 'playful', True),
+    ('Wendy', 3 ,'F', 'black', 'cute', True),
+    (NULL, 3 ,'F', 'black', 'cute', True)
+
+
+/*INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Mr. Chocolate', 7, 'M', 'brown', 'relaxed', TRUE),
+    ('Rowdy', 2, 'M', 'black', 'rowdy', TRUE),
+    ('Tabitha', 1.5, 'F', 'white', 'easy going', TRUE),
+    ('Sergeant Brown', 18, 'M', 'brown', 'funny', FALSE),
+    ('Melissa', 14, 'F', 'black', 'aggressive', TRUE),
+    ('Grinch', 4, 'M', 'brown', 'mischievious', TRUE),
+    ('Wendy', 10, 'F', 'panda', 'calm', TRUE),
+    (NULL, 0, NULL, NULL, NULL, NULL) */
